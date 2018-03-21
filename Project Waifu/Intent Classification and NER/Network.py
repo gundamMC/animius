a18fe54bd10221199ea306786ec497bd8e834d9f
gundamMC <gundammc@qq.com>
2018-03-20 17:53:34 -0700

    Joint Bi-LSTM Untested, probably will not work for now.

--- Project Waifu/Intent Classification and NER/Network.py
+++ Project Waifu/Intent Classification and NER/Network.py
@@ -1,6 +1,44 @@
 import tensorflow as tf
 import numpy as np
 
+
+def shuffle(X, Y1, Y2):
+    permutation = list(np.random.permutation(X.shape[0]))
+    shuffled_X = X[permutation]
+    shuffled_Y1 = Y1[permutation]
+    shuffled_Y2 = Y2[permutation]
+    return shuffled_X, shuffled_Y1, shuffled_Y2
+
+
+def random_mini_batches(X, Y1, Y2, mini_batch_number):
+    m = X.shape[0]
+    mini_batches_X = []
+    mini_batches_Y1 = []
+    mini_batches_Y2 = []
+
+    shuffled_X, shuffled_Y1, shuffled_Y2 = shuffle(X, Y1, Y2)
+
+    mini_batch_size = np.math.floor(m / mini_batch_number)
+
+    for batch in range(0, mini_batch_number):
+        mini_batch_X = shuffled_X[batch * mini_batch_size: (batch + 1) * mini_batch_size]
+        mini_batch_Y1 = shuffled_Y1[batch * mini_batch_size: (batch + 1) * mini_batch_size]
+        mini_batch_Y2 = shuffled_Y2[batch * mini_batch_size: (batch + 1) * mini_batch_size]
+        mini_batches_X.append(mini_batch_X)
+        mini_batches_Y1.append(mini_batch_Y1)
+        mini_batches_Y2.append(mini_batch_Y2)
+
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[mini_batch_number * mini_batch_size:]
+        mini_batch_Y1 = shuffled_Y1[mini_batch_number * mini_batch_size:]
+        mini_batch_Y2 = shuffled_Y2[mini_batch_number * mini_batch_size:]
+        mini_batches_X.append(mini_batch_X)
+        mini_batches_Y1.append(mini_batch_Y1)
+        mini_batches_Y2.append(mini_batch_Y2)
+
+    return mini_batches_X, mini_batches_Y1, mini_batch_Y2
+
+
 # hyperparameters
 learning_rate = 0.01
 epochs = 1000
@@ -64,3 +102,46 @@ def network(X):
     outputs_entities = tf.matmul(output_bw, weights["out_entities"])  # not sure if this will work...
 
     return outputs_intent, outputs_entities  # linear/no activation as there will be a softmax layer
+
+
+logits = network(x)
+
+prediction = tf.nn.softmax(network(x))
+
+# optimize
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits[0], labels=y_intent)) + \
+       tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits[1], labels=y_entities))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(cost)
+
+# initialize variables
+init = tf.global_variables_initializer()
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+# Start training
+with tf.Session(config=config) as sess:
+
+    print("start training")
+    sess.run(init)
+
+    for epoch in range(epochs + 1):  # since range is exclusive
+
+        mini_batches_X, mini_batches_Y_intent, mini_batches_Y_entities = random_mini_batches(None, None, None)
+
+        for i in range(0, len(mini_batches_X)):
+
+            batch_x = mini_batches_X[i]
+            batch_y_intent = mini_batches_Y_intent[i]
+            batch_y_entities = mini_batches_Y_entities[i]
+
+            _ = sess.run([optimizer], feed_dict={x: batch_x, y_intent: batch_y_intent, y_entities: batch_y_entities})
+
+            if epoch % display_step == 0:
+                cost_value = sess.run([cost],
+                                      feed_dict={x: batch_x, y_intent: batch_y_intent, y_entities: batch_y_entities})
+
+                print('epoch', epoch, '- cost', cost_value)
+
