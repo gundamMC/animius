Fixes ModelClasses concatenating the wrong values

ProjectWaifu/ModelClasses.py
@@ -32,16 +32,21 @@ class ChatbotData(Data):
     def add_output_data(self, output_data, output_length):
         assert (isinstance(output_data, np.ndarray))
         assert (isinstance(output_length, np.ndarray))
-        self.values['y'] = np.concatenate([self.values['x'], output_data])
-        self.values['y_length'] = np.concatenate([self.values['x_length'], output_length])
-        self.values['y_target'] = np.concatenate([output_data[:, 1:], np.full([output_data.shape[0], 1], self.values['embedding'].EOS)], axis=1)
+        self.values['y'] = np.concatenate([self.values['y'], output_data])
+        self.values['y_length'] = np.concatenate([self.values['y_length'], output_length])
+        self.values['y_target'] = np.concatenate([
+            self.values['y_length'],
+            np.concatenate([
+                output_data[:, 1:],
+                np.full([output_data.shape[0], 1], self.values['embedding'].EOS)], axis=1)
+        ])
 
     def parse_input(self, input_x):
         x, x_length, _ = ChatbotParse.sentence_to_index(ChatbotParse.split_sentence(input_x.lower()),
                                                         self.values['embedding'].words_to_index)
 
         self.values['x'] = np.concatenate([self.values['x'], np.array(x)])
-        self.values['x_length'] = np.concatenate([self.values['x'], np.array(x_length)])
+        self.values['x_length'] = np.concatenate([self.values['x_length'], np.array(x_length)])
 
     def parse_input_file(self, path):
         x = []
@@ -55,7 +60,7 @@ class ChatbotData(Data):
             x_length.append(length_tmp)
 
         self.values['x'] = np.concatenate([self.values['x'], np.array(x)])
-        self.values['x_length'] = np.concatenate([self.values['x'], np.array(x_length)])
+        self.values['x_length'] = np.concatenate([self.values['x_length'], np.array(x_length)])
 
     def parse_sentence_data(self, x, y):
         x = ChatbotParse.split_data(x)
@@ -65,10 +70,10 @@ class ChatbotData(Data):
             ChatbotParse.data_to_index(x, y, self.values['embedding'].words_to_index)
 
         self.values['x'] = np.concatenate([self.values['x'], np.array(x)])
-        self.values['y'] = np.concatenate([self.values['x'], np.array(y)])
-        self.values['x_length'] = np.concatenate([self.values['x'], np.array(x_length)])
-        self.values['y_length'] = np.concatenate([self.values['x'], np.array(y_length)])
-        self.values['y_target'] = np.concatenate([self.values['x'], np.array(y_target)])
+        self.values['y'] = np.concatenate([self.values['y'], np.array(y)])
+        self.values['x_length'] = np.concatenate([self.values['x_length'], np.array(x_length)])
+        self.values['y_length'] = np.concatenate([self.values['y_length'], np.array(y_length)])
+        self.values['y_target'] = np.concatenate([self.values['y_target'], np.array(y_target)])
 
     def add_cornell(self, conversations_path, movie_lines_path, lower_bound=None, upper_bound=None):
         x, y = ChatbotParse.load_cornell(conversations_path, movie_lines_path)