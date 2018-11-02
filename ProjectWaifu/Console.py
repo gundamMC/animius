ff5a5532aba5cfeb9f48b4e695ed4aa0f3547994
gundamMC <gundammc@qq.com>
2018-11-02 16:28:02 -0700

    Changes console input scanning behavior

--- ProjectWaifu/Console.py
+++ ProjectWaifu/Console.py
@@ -1,105 +1,128 @@
-from ProjectWaifu.SpeakerVerification.SpeakerVerificationNetwork import SpeakerVerificationModel
-from ProjectWaifu.IntentNER.IntentNERNetwork import IntentNERModel
-from shlex import split
+import ProjectWaifu.SpeakerVerification as SpeakerVerification
+import ProjectWaifu.IntentNER as IntentNER
+import ProjectWaifu.Chatbot as Chatbot
+import ProjectWaifu.ModelClasses as ModelClasses
 import ProjectWaifu.Utils as Utils
-
-Networks = {}
-SelectedNetwork = None
+from shlex import split as shell_split
 
 
-def ParseArgs(InputString):
-    return split(InputString)
+class Console:
 
+    Networks = {}
+    SelectedNetwork = None
 
-def GetNetworks(args):
-    Utils.printMessage("Current networks:")
-    for name, value in Networks.items():
-        Utils.printMessage(name + ' - ' + type(value).__name__)
+    @staticmethod
+    def GetNetworks(args):
+        print("Current networks:")
+        for name, value in Networks.items():
+            print(name + ' - ' + type(value).__name__)
 
+    @staticmethod
+    def Select(args):
+        if len(args) != 1:
+            print("Usage: Select <Network Name>")
+            return
+        if args[0] not in Networks:
+            print("Error: Network \"" + args[0] + "\" does not exist")
+            return
+        global SelectedNetwork
+        SelectedNetwork = Networks[args[0]]
 
-def Select(args):
-    if len(args) != 1:
-        Utils.printMessage("Usage: Select <Network Name>")
-        return
-    if args[0] not in Networks:
-        Utils.printMessage("Error: Network \"" + args[0] + "\" does not exist")
-        return
-    global SelectedNetwork
-    SelectedNetwork = Networks[args[0]]
+    @staticmethod
+    def GetSelected(args):
+        return SelectedNetwork.name
 
+    @staticmethod
+    def AddNetwork(args):
 
-def GetSelected(args):
-    return SelectedNetwork.name
+        if len(args) != 2:
+            print("Usage: AddNetwork <Network Type> <Network Name>")
+            return
 
+        if args[0].lower() == "speakerverification":
+            Networks[args[1]] = SpeakerVerificationModel()
+            print("Speaker verification network \"" + args[1] + "\" added and selected!")
 
-def AddNetwork(args):
+        elif args[0].lower() == "intentandner":
+            Networks[args[1]] = IntentNERModel()
+            print("Intent and NER network \"" + args[1] + "\" added and selected!")
 
-    if len(args) != 2:
-        Utils.printMessage("Usage: AddNetwork <Network Type> <Network Name>")
-        return
+        else:
+            print("Invalid type")
+            return
 
-    if args[0].lower() == "speakerverification":
-        Networks[args[1]] = SpeakerVerificationModel()
-        Utils.printMessage("Speaker verification network \"" + args[1] + "\" added and selected!")
+        Select([args[1]])
 
-    elif args[0].lower() == "intentandner":
-        Networks[args[1]] = IntentNERModel()
-        Utils.printMessage("Intent and NER network \"" + args[1] + "\" added and selected!")
+    @staticmethod
+    def SetTrainingData(args):
 
-    else:
-        Utils.printMessage("Invalid type")
-        return
+        if len(args) < 1:
+            print("Usage: SetTrainingData {<Network Arguments>}")
+            return
 
-    Select([args[1]])
+        SelectedNetwork.setTrainingData(args)
+        print("Training data set!")
 
+    @staticmethod
+    def Train(args):
 
-def SetTrainingData(args):
+        if len(args) > 2:
+            print("Usage: Train [Epochs] [Display step]")
+            return
 
-    if len(args) < 1:
-        Utils.printMessage("Usage: SetTrainingData {<Network Arguments>}")
-        return
+        if len(args) == 0:
+            SelectedNetwork.train()
+        elif len(args) == 1:
+            SelectedNetwork.train(epochs=int(args[0]))
+        else:
+            SelectedNetwork.train(epochs=int(args[0]), display_step=int(args[1]))
 
-    SelectedNetwork.setTrainingData(args)
-    Utils.printMessage("Training data set!")
+    @staticmethod
+    def Predict(args):
+        if len(args) != 1:
+            print("Usage: Predict <Network input>")
+            return
+        output = SelectedNetwork.predict(args[0])
+        print(output)
 
+    @staticmethod
+    def PredictAll(args):
+        if len(args) == 1:
+            output = SelectedNetwork.predictAll(args[0])
+        elif len(args) == 2:
+            output = SelectedNetwork.predictAll(args[0], args[1])
+        else:
+            print("Usage: Predict <Path of file containing network input>")
+            return
+        print(output)
 
-def Train(args):
+    @staticmethod
+    def LoadWordVector(args):
+        if len(args) == 1:
+            Utils.createEmbedding(args[0])
+        else:
+            print("Usage: LoadWordVector <Path of word vector>")
+            return
+            print(output)
 
-    if len(args) > 2:
-        Utils.printMessage("Usage: Train [Epochs] [Display step]")
-        return
+    commands = {
+        's': AddNetwork
+    }
 
-    if len(args) == 0:
-        SelectedNetwork.train()
-    elif len(args) == 1:
-        SelectedNetwork.train(epochs=int(args[0]))
-    else:
-        SelectedNetwork.train(epochs=int(args[0]), display_step=int(args[1]))
+    @staticmethod
+    def ParseArgs(input_string):
+        try:
+            input_string = shell_split(input_string)
+        except Exception as e:
+            print(e)
+            return None, None
+        command = input_string[0]
+        args = {}
+        for arg in input_string[1:]:
+            if not str.startswith(arg, '-') or '=' not in arg:
+                print('Invalid argument', arg, '. Arguments must start with - or -- and include a =')
+                return None, None
+            arg_name, arg_value = arg.lstrip('-').split('=')
+            args[arg_name] = arg_value
 
-
-def Predict(args):
-    if len(args) != 1:
-        Utils.printMessage("Usage: Predict <Network input>")
-        return
-    output = SelectedNetwork.predict(args[0])
-    Utils.printMessage(output)
-
-
-def PredictAll(args):
-    if len(args) == 1:
-        output = SelectedNetwork.predictAll(args[0])
-    elif len(args) == 2:
-        output = SelectedNetwork.predictAll(args[0], args[1])
-    else:
-        Utils.printMessage("Usage: Predict <Path of file containing network input>")
-        return
-    Utils.printMessage(output)
-
-
-def LoadWordVector(args):
-    if len(args) == 1:
-        Utils.createEmbedding(args[0])
-    else:
-        Utils.printMessage("Usage: LoadWordVector <Path of word vector>")
-        return
-        Utils.printMessage(output)
+        return command, args
