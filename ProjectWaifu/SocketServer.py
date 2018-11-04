d0e9322226c445ef3c95e22285730a00e760a1f9
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2018-11-05 07:33:25 +0800

    Updated
    Socketserver

--- ProjectWaifu/SocketServer.py
+++ ProjectWaifu/SocketServer.py
@@ -1,43 +1,43 @@
-import socket
-import ProjectWaifu.Console as Console
-import ProjectWaifu.Utils as Utils
+from .SocketServerModel import client,Request,Response,AEScipher
 
-s = socket.socket()
-host = "localhost"
-port = 8089
-s.bind((host, port))
-s.listen(1)
+clients = {}
 
-while True:
-    conn, addr = s.accept()
 
-    print('New connection from %s:%d' % (addr[0], addr[1]))
+def new_client(c):
+    try:
+        #print("%s(%s) 尝试连接" % (c.addr, c.port))
+        while True:
+            req = c.recv()
+            print(req)
+    except socket.errno as e:
+        print("Socket error: %s" % str(e))
+    except Exception as e:
+        print("Other exception: %s" % str(e))
+    finally:
+        #print("%s(%s) 断开连接" % (c.addr, c.port))
+        c.close()
 
-    Utils.setSocket(conn)
+
+def start_server(port, local=True, cnum=10):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    if local:
+        host = "127.0.0.1"
+    else:
+        host = socket.gethostname()
+    server.bind((host, port))
+
+    # 侦听客户端
+    server.listen(cnum)
+    print("服务器已开启")
 
     while True:
-        try:
-            data = conn.recv(1024)
-            data = data.decode("utf-8").strip()
-            if not data or data == "":
-                break
-            elif data == 'exit':
-                conn.close()
-                break
-            else:
-                InputArgs = Console.ParseArgs(data)
-                Command = InputArgs[0]
-                InputArgs = InputArgs[1:]
+        # 接受客户端连接
+        conn, addr = server.accept()
+        c = client(conn, addr)
+        t = threading.Thread(target=new_client, args=(c,))
+        t.start()
 
-                method_to_call = getattr(Console, Command, None)
-
-                if method_to_call is None:
-                    Utils.printMessage("Invalid command")
-                else:
-                    method_to_call(InputArgs)
-
-                Utils.printMessage("end")
-
-        except socket.timeout:
-            print('Connection timed out')
-            continue
+if __name__ == "__main__":
+    start_server(12345, local=False)
+    #print("服务器已关闭")
