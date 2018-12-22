9478470c4abbb5a2b5279ebfa396f9ee8e02440f
gundamMC <gundammc@qq.com>
2018-12-22 14:50:14 -0800

    Changes Waifu object to use CombinedPredictionModel

--- animius/__init__.py
+++ animius/__init__.py
@@ -2,6 +2,7 @@ from animius.Model import Model
 from animius.ModelData import *
 from animius.ModelConfig import *
 from animius.WordEmbedding import WordEmbedding
+from animius.Waifu import Waifu
 import animius.Utils as Utils
 
 import animius.IntentNER as IntentNER
