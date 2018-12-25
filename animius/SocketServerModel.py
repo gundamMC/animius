908d65ad26adfc88d6eda36106aa45f95531419e
Xiaoyang Liu <siujoeng.lau@gmail.com>
2018-12-26 00:42:31 +0800

    Wait For Password After Connection
    Signed-off-by: xAsiimov <sabervateurs@gmail.com>

--- animius/SocketServerModel.py
+++ animius/SocketServerModel.py
@@ -75,9 +75,10 @@ class AEScipher:
 
 
 class Client:
-    def __init__(self, socket, addr):
+    def __init__(self, socket, addr, pwd):
         self.addr = addr[0]
         self.port = addr[1]
+        self.pwd=pwd
         self.socket = socket
         self.AEScipher = None
 
