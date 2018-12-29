f90746a1eadfac4b46c1c942163d157ca4239da1
gundamMC <gundammc@qq.com>
2018-12-28 21:53:53 -0800

    Fix import order of __init__.py Optimizing imports in d55608b breaks the import order of __init__.py

--- animius/__init__.py
+++ animius/__init__.py
@@ -1,9 +1,13 @@
+from animius.Model import Model
+from animius.ModelConfig import *
+from animius.WordEmbedding import WordEmbedding
+from animius.ModelData import *
+from animius.Console import Console
+
 import animius.Chatbot as Chatbot
 import animius.IntentNER as IntentNER
 import animius.SpeakerVerification as SpeakerVerification
 import animius.Utils as Utils
-from animius.Model import Model
-from animius.ModelConfig import *
-from animius.ModelData import *
+
 from animius.Waifu import Waifu
-from animius.WordEmbedding import WordEmbedding
+
