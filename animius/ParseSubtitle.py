04ab4f6d8dd961af2d2d00253111db607d27b5ca
gundamMC <gundammc@qq.com>
2019-01-05 20:14:21 -0800

    Parse subtitles as chatbot data with speaker verification

--- animius/ParseSubtitle.py
+++ animius/ParseSubtitle.py
@@ -8,11 +8,12 @@ class Parser:
 
     def __init__(self):
         self.SSAFile = None
+        self.audio_sentences = None
 
     def load(self, path):
         self.SSAFile = pysubs2.SSAFile.load(path)
 
-    def sliceAudio(self, path):
+    def slice_audio(self, path):
         print("Processing " + path)
 
         audio = AudioSegment.from_file(path)
@@ -26,15 +27,16 @@ class Parser:
             segment = audio[sub.start:sub.end]
             segment.export(savePath + str(index).zfill(4) + ".wav", format="wav")
             index += 1
+            self.audio_sentences.append(sub.plaintext)
 
         print("Done!")
 
-    def detectConversation(self, speaking):
+    def detect_conversation(self, speaking):
         window = 5
         conversation = []
 
         if isinstance(speaking, str):
-            with open(speaking) as file:
+            with open(speaking, 'r') as file:
                 lines = file.read().splitlines()
                 speaking = []
                 for line in lines:
@@ -42,6 +44,7 @@ class Parser:
 
         print(speaking)
 
+        # get conservation frames by comparing the is-speaker and not-speaker labels within a window
         for i in range(0, len(speaking) - window, window):
             TrueCount = 0
             FalseCount = 0
@@ -55,23 +58,70 @@ class Parser:
 
         result = []
         startingIndex = 0
+        # make connecting conversation frames as one large group
         while startingIndex < len(conversation) - 1:
             for i in range(startingIndex + 1, len(conversation)):
-                if conversation[i][0] != conversation[i - 1][1]:  # if the ending does not follow the previous start
+                if conversation[i][0] != conversation[i - 1][1]:
+                    # if the ending does not follow the previous start, the conversation is not connected
                     result.append([conversation[startingIndex][0], conversation[i - 1][1]])
                     break
                 elif i == len(conversation) - 1:
+                    # if it has reached the end of conversations
                     result.append([conversation[startingIndex][0], conversation[i - 1][1]])
                     break
-            startingIndex = i
+                else:
+                    continue
+            startingIndex = i  # start from where the for loop left off
 
-        return result
+        return result  # a list of [start, end]'s
 
-    def getConversation(self, conversations):
+    def get_conversation_sentences(self, conversations):
+        # return the actual sentences in conversations
         sentences = []
         for conv in conversations:
             conv_sentences = []
             for i in range(conv[0], conv[1] + 1):
-                conv_sentences.append(self.SSAFile[i].plaintext)
+                conv_sentences.append(self.audio_sentences[i])
             sentences.append(conv_sentences)
         return sentences
+
+    def get_chatbot_data(self, is_speaker):
+        conversations = self.detect_conversation(is_speaker)
+        queries = []
+        responses = []
+
+        for conv in conversations:
+
+            i = conv[0]
+            start_index = None  # the index where the first non-speaker speaks
+
+            while i <= conv[1]:
+                if not is_speaker[i]:
+                    start_index = i
+                    break
+
+            if start_index is None:
+                # the 'conversation' for some reason is a monologue of the speaker
+                continue
+
+            i = start_index
+            last_is_speaker = False
+            not_speaker_query = self.audio_sentences[i]
+            speaker_response = ''
+
+            while i <= conv[1]:
+                if is_speaker[i]:
+                    speaker_response += self.audio_sentences[i]
+                    last_is_speaker = True
+                else:
+                    # switch from speaker to non-speaker = the end of a query and response set
+                    if last_is_speaker is True:
+                        queries.append(not_speaker_query)
+                        responses.append(speaker_response)
+                        not_speaker_query = ''
+                        speaker_response = ''
+
+                    not_speaker_query += self.audio_sentences[i]
+                    last_is_speaker = False
+
+        return queries, responses  # add to chatbot data with add_parse_sentences
