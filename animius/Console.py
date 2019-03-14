6991e5f62337ff3e581bb667ee9902e4057f4487
gundamMC <gundammc@qq.com>
2019-03-14 15:17:39 -0700

    Move Main.py to am.console.start()

--- animius/Console.py
+++ animius/Console.py
@@ -1930,3 +1930,30 @@ i
                         raise exc
             else:
                 print('Invalid command')
+
+    @staticmethod
+    def start():
+        import readline
+
+        console = am.Console()
+        console.init_commands()
+
+        def completer(user_input, state):
+            options = [i for i in console.commands if i.startswith(user_input)]
+            if state < len(options):
+                return options[state]
+            else:
+                return None
+
+        readline.parse_and_bind("tab: complete")
+        readline.set_completer(completer)
+
+        print("Animius. Type 'help' or '?' to list commands. Use 'exit' to break")
+
+        while True:
+            user_input = input('Input: ')
+
+            if user_input.lower() == 'exit':
+                break
+
+            console.handle_command(user_input)
