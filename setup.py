2c3eb0af42b419338e8f62528c473c5795ee5d6e
gundamMC <gundammc@qq.com>
2019-03-15 17:47:10 -0700

    Update setup.py to include more arguments

--- setup.py
+++ setup.py
@@ -4,19 +4,32 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='animius',
-     version='1.0',
-     author="gundamMC",
-     author_email="gundamMC@gundamMC.com",
-     description="A deep-learning virtual assistant engine",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/gundamMC/Animius",
-     packages=setuptools.find_packages(),
-     python_requires='>=3',
-     install_requires=['numpy', 'pysubs2', 'pydub', 'scipy', 'speechpy'],
-     extras_require={
+    name='animius',
+    version='1.0.0a1',
+    author="gundamMC",
+    author_email="gundamMC@gundamMC.com",
+    description="A deep-learning virtual assistant engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://animius.org",
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    install_requires=['numpy', 'pysubs2', 'pydub', 'scipy', 'speechpy'],
+    extras_require={
         "tf": ["tensorflow==1.12.0"],
         "tf_gpu": ["tensorflow-gpu==1.12.0"],
-     }  # tf 2.0 not supported for now
+    },  # tf 2.0 not supported for now
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    license='Apache 2.0',
+    keywords='animius ai virtual assistant deep learning',
 )
