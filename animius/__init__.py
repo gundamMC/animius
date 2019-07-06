92bf90172af0eb6c604c4dc8a604d2a05e172501
gundamMC <gundammc@gundammc.com>
2019-07-06 00:46:25 -0400

    Fix __init__.py order

--- animius/__init__.py
+++ animius/__init__.py
@@ -1,12 +1,19 @@
+# Please DONT "optimize" this file
+# PyCharm can't seem to learn the import order and will
+# sort them out alphabetically
+
+from animius.Model import Model
+from animius.ModelConfig import *
+from animius.WordEmbedding import WordEmbedding
+from animius.ModelData import *
+from animius.Console import Console
+from animius.Commands import Commands
+
 import animius.Chatbot as Chatbot
 import animius.IntentNER as IntentNER
 import animius.SpeakerVerification as SpeakerVerification
 import animius.Utils as Utils
-from animius.Commands import Commands
-from animius.Console import Console
-from animius.Model import Model
-from animius.ModelConfig import *
-from animius.ModelData import *
-from animius.ParseSubtitle import Parser as SubtitleParser
+
 from animius.Waifu import Waifu
-from animius.WordEmbedding import WordEmbedding
+
+from animius.ParseSubtitle import Parser as SubtitleParser
